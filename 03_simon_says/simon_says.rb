65cd277369f3417549ed93878a53aa34b31b78e6
Michael Long <mikeylong04@googlemail.com>
2017-08-04 17:20:55 +0100

    Completed tests 3 and 4

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
 #write your code here
+def echo phrase
+	phrase
+end
+
+def shout phrase
+	phrase.upcase
+end
+
+def repeat phrase, times = 2
+		"#{phrase} " * (times - 1) + "#{phrase}"
+end
+
+def start_of_word word, position
+	word[0, position]
+end
+
+def first_word phrase
+	space = phrase.index(" ")
+	phrase[0, space]
+end
+
+def titleize phrase
+	array = phrase.split
+	string = ""
+	array.each_with_index do |word, index|
+		if index == 0 or index == array.length - 1
+			string += word.capitalize + " "
+		else
+			case word.downcase
+			when "and", "or", "over", "the", "in", "to", "for", "of" "a"
+				string += word + " "
+			else
+				string += word.capitalize + " "
+			end
+		end
+	end
+	string[0, string.length - 1]
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
 #write your code here
+def translate phrase
+	phrase.downcase!
+	string = ""
+	array_of_words = phrase.split(" ")
+	array_of_words.each do |word|
+		array_of_letters = word.split("")
+		until array_of_letters[0] == "a" or array_of_letters[0] == "e" or array_of_letters[0] == "i" or array_of_letters[0] == "o" or array_of_letters[0] == "u"
+			if array_of_letters[0] == "q" and array_of_letters[1] == "u"
+				first = array_of_letters.shift
+				second = array_of_letters.shift
+				array_of_letters.push(first)
+				array_of_letters.push(second)
+			else
+				first = array_of_letters.shift
+				array_of_letters.push(first)
+			end
+		end
+		string += array_of_letters.join + "ay "
+	end
+	string[0, string.length - 1]
+end

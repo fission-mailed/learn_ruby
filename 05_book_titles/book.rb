6a02857c8f3db3c0a20ea7edfe78c8ec92e447e9
Michael Long <mikeylong04@googlemail.com>
2017-08-08 01:40:29 +0100

    Completed problem 5

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,23 @@
 class Book
 # write your code here
+	attr_reader :title
+	
+	def title=(book_title)
+		@title = book_title.downcase
+		words = @title.split(" ")
+		words.each_with_index do |word, index|
+			if index == 0
+				word.capitalize!
+			else
+				case word
+				when "a", "an", "and", "of", "in", "the"
+					word
+				else
+					word.capitalize!
+				end
+			end
+		end
+		
+		@title = words.join(" ")
+	end
 end
